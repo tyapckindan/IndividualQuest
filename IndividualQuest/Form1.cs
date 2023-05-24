576bfea79d21a0b6bb580f7f2a3801a909a61f3f
Данила Тяпкин <dtapkin6@gmail.com>
2023-05-24 10:04:26 +0300

    Удалил ненужные директивы using

--- IndividualQuest/Form1.cs
+++ IndividualQuest/Form1.cs
@@ -1,15 +1,6 @@
 ﻿using System;
-using System.Data;
-using System.Data.OleDb;
 using System.IO;
 using System.Windows.Forms;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using Microsoft.Office.Interop.Excel;
 
 namespace IndividualQuest
 {
